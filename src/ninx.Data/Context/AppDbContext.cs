b5c44ddec8138a07e0bcb3a13984291f50a644a6
Matheus Silva <matheus.silva@bit.com.br>
2026-03-20 13:44:23 -0300

    Adicionando EntityFrameWork

---
+++ src/ninx.Data/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ninx.Domain.Entities;
+
+namespace ninx.Data.Context
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assinatura> Assinaturas { get; set; }
+        public DbSet<CategoriaProduto> CategoriaProduto { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
+        public DbSet<ItemVenda> ItemVendas { get; set; }
+        public DbSet<MovimentacaoEstoque> MovimentacaoEstoque { get; set; }
+        public DbSet<PagamentoFiado> PagamentoFiados { get; set; }
+        public DbSet<PagamentoVenda> PagamentoVendas { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<SessaoWhatsapp> sessaoWhatsapps { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }    
+        public DbSet<UsuarioComercio> UsuarioComercio { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<VendaFiado> vendaFiados { get; set; }
+    }
+}
